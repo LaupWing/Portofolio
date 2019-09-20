4db1c877ffdc03fc588e203ea7d9ac3c93afdab2
LaupWing <loc_strict@hotmail.com>
2019-09-20 18:02:07 +0200

    Testing data

--- src/components/Sections/Home/Github/day.vue
+++ src/components/Sections/Home/Github/day.vue
@@ -23,14 +23,14 @@
 <script>
 export default {
     name: 'day',
-    props:['daySize', 'day'],
+    props:['daySize', 'day', 'iets'],
     data(){
         return{
 
         }
     },
     methods:{
-        
+
     }
 }
 </script>
