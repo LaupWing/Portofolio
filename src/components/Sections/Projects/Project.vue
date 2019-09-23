88f2459cd4cba17e2ef94603c678e02b34c45195
LaupWing <loc_strict@hotmail.com>
2019-09-23 15:15:42 +0200

    adding: Project panel

---
+++ src/components/Sections/Projects/Project.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="project">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Project',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
