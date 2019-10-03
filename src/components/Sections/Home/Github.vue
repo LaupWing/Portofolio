1646cdc2acd2db42f3faef7b31e29c974fc1bbff
LaupWing <loc_strict@hotmail.com>
2019-10-03 19:14:36 +0200

    right day is desplayed now only the right n of r m

--- src/components/Sections/Home/Github.vue
+++ src/components/Sections/Home/Github.vue
@@ -137,7 +137,7 @@ export default {
             const year= date.getFullYear()
             for(let i=0; i<10; i++){
                 this.timespan.push({
-                    day: day - i > 0 ? day - i : (day-i) > 0 ? (this.getLastDayOfMonth(month) - i) : this.getLastDayOfMonth(month),
+                    day: day - i > 0 ? day - i : (day-i) < 0 ? ((this.getLastDayOfMonth(month) + (day-i))) : this.getLastDayOfMonth(month),
                     year: month === 1 && day === 1 ? year-1 : year, // this formule isnt correct yet maybe in the future i will change it NOTE
                     month: day - i > 0 ? month : month === 1 ? 12 : month -1  
                 })
